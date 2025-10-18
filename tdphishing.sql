48bdb07d592e65aa6ffb07c4aefa7b3d512ea00b
ayaoujaa <ayaoujaa194@gmail.com>
2025-10-18 22:14:15 +0200

    Add files via upload

---
+++ tdphishing.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : sam. 18 oct. 2025 à 20:13
+-- Version du serveur : 5.7.24
+-- Version de PHP : 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tdphishing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `mdp` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `login`, `mdp`) VALUES
+(1, 'ayaoujaa194@gmail.com', 'azerty'),
+(2, 'ismaelzerouali@gmail.com', 'gz'),
+(3, 'ismaelzerouali@gmail.com', 'azerty'),
+(4, 'ismaelzerouali@gmail.com', 'azerty'),
+(5, 'yanisdenanterrelafolie', 'avn,'),
+(6, 'dfv', 'dv'),
+(7, 'prof.gravouil', 'testtest'),
+(8, 'jntt_782', 'Jannate?'),
+(9, 'nla_9500', 'Naila2009*');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
